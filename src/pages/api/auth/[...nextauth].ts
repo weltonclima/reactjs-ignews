dbc4051edb3651e21fa529817ba317a7ee2dcd34
Welton Lima <wlima@epharma.com.br>
2021-04-04 19:33:26 -0300

    faunaDb validations

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth";
+import NextAuth from 'next-auth';
 import { query as q } from 'faunadb';
-import Providers from "next-auth/providers";
-import { fauna } from "../../../services/fauna";
+import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
 
@@ -14,16 +14,35 @@ export default NextAuth({
   ],
   callbacks: {
     async signIn(user, account, profile) {
-      const { email } = user;
+      const { email } = user
 
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: { email } }
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
         )
-      )
-
-      return true
+        return true;
+      } catch {
+        return false;
+      }
     },
   }
 })
