88b4c4fae0dcf4abacedc55e732fdc7c7cfdb957
Welton Lima <wlima@epharma.com.br>
2021-04-04 11:42:10 -0300

    Initial commitment changing to typescript

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,13 @@
+
+import styles from "./styles.module.scss"
+
+export function SubscriberButton(){
+  return(
+    <button
+      type="button"
+      className={styles.sbscriberButton}
+    >
+      Subscribe Now
+    </button>
+  );
+} 
