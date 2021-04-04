e245aed7e0f7ae192271704555b2fbbf6095cd8a
Welton Lima <wlima@epharma.com.br>
2021-04-04 16:00:13 -0300

    adding faunaDb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,29 @@
 import NextAuth from "next-auth";
+import { query as q } from 'faunadb';
 import Providers from "next-auth/providers";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
 
-  providers:[
+  providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: { email } }
+        )
+      )
+
+      return true
+    },
+  }
 })
