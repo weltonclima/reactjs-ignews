1b25a00c338a2f2a676c1fce6bd8976270bd01a7
Welton Lima <wlima@epharma.com.br>
2021-04-04 21:23:29 -0300

    adding checkout stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,35 @@
 
+import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss"
 
-export function SubscriberButton(){
-  return(
+export function SubscriberButton() {
+  const [session] = useSession();
+
+  async function handlesubscriber() {
+    if (!session) {
+      return signIn('github');
+    }
+
+    try {
+      const { data } = await api.post('/subscriber');
+      const { sessionId } = data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({sessionId});
+    }catch (err) {
+      alert(err.message);
+    }
+  }
+
+  return (
     <button
       type="button"
       className={styles.sbscriberButton}
+      onClick={handlesubscriber}
     >
       Subscribe Now
     </button>
   );
-} 
+}
